f91d5daa8ddf3379a8a2becc26c844b2c0c37e5c
wendybast <64814804+wendybast@users.noreply.github.com>
2020-06-03 18:41:32 -0300

    completando semana

--- src/pkgswitch/Switch.java
+++ src/pkgswitch/Switch.java
@@ -17,17 +17,34 @@ public class Switch {
     public static void main(String[] args) {
         // TODO code application logic here
         int numDia;
-        numDia=2;
+        
+        numDia=1;
         
         switch(numDia){
             case 1:
                 System.out.println("Lunes");
-                
-                
-        }
-        
-        
-        
+                break;
+            case 2;
+                 System.out.println("Martes");
+                break;
+            case 2;
+                 System.out.println("Miercoles");
+                break;
+            case 4;
+                 System.out.println("Jueves");
+                break;
+            case 5;
+                 System.out.println("Viernes");
+                break;
+            case 6;
+                 System.out.println("Sabado");
+                break;
+            case 7; 
+                 System.out.println("Domingo");
+                break;
+            Default:
+                 System.out.println("Dia de la semana no valido");
+        ]     
     }
     
 }
